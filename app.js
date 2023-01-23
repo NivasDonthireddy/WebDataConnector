4f213c92c917193f94d264a46fac570cdd69a33a
Nivas Donthireddy <dlnivas@hotmail.com>
2023-01-23 23:21:56 +0530

    test

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('',(req,res)=>{
+app.get('/test',(req,res)=>{
   console.log('Hello')
 })
 
